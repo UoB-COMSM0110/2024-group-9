0ffab9073da5f3b90d2e85fc53bcb491fc76d9c4
eelliott-hub <elizabeth.g.elliott@gmail.com>
2024-02-29 14:18:46 +0000

    Additional attributes and methods for Level - height and width - notes about how to store info on sprites

--- platformer/src/main/java/Level.java
+++ platformer/src/main/java/Level.java
@@ -4,7 +4,18 @@ public class Level {
     private boolean started;
     private int score;
 
+    // Set overall space available for the level
+    private int height;
+    private int width;
+
     // Need some way of storing the map for the level - location of different sprites
+    // ArrayLists of Sprites? Would need Sprites to have co-ordinates within the level space.
+    // When a level is instantiated an ArrayList of Sprites is generated for that level.
+    // How to access the sprites to change their attributes?
+
+    // private ArrayList<Sprite> sprites;
+    
+    private String backgroundColour; // Probably change this depending on how drawing is done
 
     // Constructor method
     public Level(){
@@ -27,6 +38,22 @@ public class Level {
 
     public void setScore(int newScore){
         score = newScore;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void setHeight(int newHeight){
+        height = newHeight;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public void setWidth(int newWidth){
+        width = newWidth;
     }
 
 }
