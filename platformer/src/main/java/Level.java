00645df124d66116a4a3d2386d4f7d8ffe33240b
eelliott-hub <elizabeth.g.elliott@gmail.com>
2024-03-01 14:59:40 +0000

    Updates to Level - JSON file to store level info

--- platformer/src/main/java/Level.java
+++ platformer/src/main/java/Level.java
@@ -1,26 +1,59 @@
 package main.java;
 
-public class Level {
+import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+
+
+public class Level() {
+    private String domain;
+    private final PApplet parent;
     private boolean started;
     private int score;
 
     // Set overall space available for the level
     private int height;
     private int width;
-
-    // Need some way of storing the map for the level - location of different sprites
-    // ArrayLists of Sprites? Would need Sprites to have co-ordinates within the level space.
-    // When a level is instantiated an ArrayList of Sprites is generated for that level.
-    // How to access the sprites to change their attributes?
-
-    // private ArrayList<Sprite> sprites;
-    
-    private String backgroundColour; // Probably change this depending on how drawing is done
+    private int backgroundColour1;
+    private int backgroundColour2;
+    private int backgroundColour3;
 
     // Constructor method
-    public Level(){
+    public Level(String jsonFilePath, PApplet parent) {
+        this.parent = parent;
         started = true;
         score = 0;
+        // Load data from JSON file
+        JSONObject json = parent.loadJSONObject(jsonFilePath);
+
+        // Level width, height and background colour
+        height = json.getInt("height");
+        width = json.getInt("width");
+        backgroundColour1 = json.getInt("backgroundColour1");
+        backgroundColour2  = json.getInt("backgroundColour2");
+        backgroundColour3 = json.getInt("backgroundColour3");
+
+        // Sprites
+        JSONArray spriteData = (JSONArray) json.get("sprites");
+        Sprite sprites[] = new Sprite[spriteData.size()];
+
+        for (int i = 0; i < spriteData.size(); i++) {
+            // Get each object in the array
+            JSONObject sprite = spriteData.getJSONObject(i);
+            // Get a position object
+            JSONObject position = sprite.getJSONObject("position");
+            // Get xPos, yPos from position
+            int xPos = position.getInt("xPos");
+            int yPos = position.getInt("yPos");
+            // Get sprite width, height and layer from sprite
+            int spriteWidth = sprite.getInt("spriteWidth");
+            int spriteHeight = sprite.getInt("spriteHeight");
+            int spriteLayer = sprite.getInt("layer");
+
+            // Put objects in array
+            sprites[i] = new Sprite(xPos, yPos, spriteWidth, spriteHeight, spriteLayer);
+        }
+
     }
 
     // Getter and setter methods
